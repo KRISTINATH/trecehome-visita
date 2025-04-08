fd12a6f81747d85de01eed3e0e42eac60efdea8f
KRISTINATH <kristina.trecehome@gmail.com>
2025-04-08 15:47:27 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', event => {
-  console.log('[ServiceWorker] Instalado');
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('trecehome-cache-v1').then(function (cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
